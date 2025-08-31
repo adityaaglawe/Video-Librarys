52c5ade1061b43bbbd44e331a45a2ef51e46b066
adityaaglawe <163434240+adityaaglawe@users.noreply.github.com>
2025-08-31 19:24:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,206 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+var mongoClient = require("mongodb").MongoClient;
+var express =require("express");
+var cors = require("cors");
 
-import reportWebVitals from './reportWebVitals';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
-import { CookiesProvider } from 'react-cookie';
-import store from './store/store';
-import { Provider } from 'react-redux';
+var app = express();
+app.use(cors());
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+var connectionString = "mongodb://127.0.0.1:27017";
+
+app.get("/get-admin",(req,res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+        var database = connectionObject.db("videodb");
+        database.collection("tbladmin").find({}).toArray().then(documents=>{
+            res.send(documents);
+            res.end();
+        });
+    });
+
+});
+
+app.get("/get-videos",(req,res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+        var database = connectionObject.db("videodb");
+        database.collection("tblvideos").find({}).toArray().then(documents=>{
+            res.send(documents);
+            res.end();
+        });
+    });
+
+});
+
+app.get("/get-categories",(req,res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+        var database = connectionObject.db("videodb");
+        database.collection("tblcategories").find({}).toArray().then(documents=>{
+            res.send(documents);
+            res.end();
+        });
+    });
+
+});
+
+app.get("/get-users",(req,res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+        var database = connectionObject.db("videodb");
+        database.collection("tblusers").find({}).toArray().then(documents=>{
+            res.send(documents);
+            res.end();
+        });
+    });
+
+});
+
+app.get("/get-user/:useeid",(req,res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+        var database = connectionObject.db("videodb");
+        database.collection("tblusers").find({UserId:req.params.useeid}).toArray().then(documents=>{
+            res.send(documents);
+            res.end();
+        });
+    });
+
+});
+
+app.get("/get-video/:id",(req,res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+        var database = connectionObject.db("videodb");
+        database.collection("tblvideos").find({VideoId:parseInt(req.params.id)}).toArray().then(documents=>{
+            res.send(documents);
+            res.end();
+        });
+    });
+
+});
+
+app.get("/filter-video/:categoryid",(req,res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+        var database = connectionObject.db("videodb");
+        database.collection("tblvideos").find({CategoryId:parseInt(req.params.categoryid)}).toArray().then(documents=>{
+            res.send(documents);
+            res.end();
+        });
+    });
+
+});
+
+app.post("/register-user", (req, res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+
+        var database = connectionObject.db("videodb");
+
+        var user = {
+             UserId: req.body.UserId,
+             UserName: req.body.UserName,
+             Password: req.body.Password,
+             Email: req.body.Email,
+             Mobile: req.body.Mobile
+        };
+
+        database.collection("tblusers").insertOne(user).then(()=>{
+             console.log(`User Registered`);
+             res.end();
+        });
+
+    });
+});
+
+app.post("/add-category", (req, res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+
+        var database = connectionObject.db("videodb");
+
+        var user = {
+             CategoryId:parseInt(req.body.CategoryId),
+             CategoryName:req.body.CategoryName
+        }
+
+        database.collection("tblcategories").insertOne(category).then(()=>{
+             console.log(`Category Added`);
+             res.end();
+        });
+
+    });
+});
+
+app.post("/add-video", (req, res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+
+        var database = connectionObject.db("videodb");
+
+        var video = {
+            VideoId: parseInt(req.body.VideoId),
+            Title: req.body.Title,
+            Url: req.body.Url,
+            Description: req.body.Description,
+            Likes: parseInt(req.body.Likes),
+            Dislikes: parseInt(req.body.Dislikes),
+            Views: parseInt(req.body.Views),
+            CategoryId: parseInt(req.body.CategoryId),
+            Comments: [req.body.Comments]
+        }
+
+        database.collection("tblvideos").insertOne(video).then(()=>{
+             console.log(`Video Added`);
+             res.end();
+        });
+
+    });
+});
+
+app.put("/edit-video/:id", (req, res)=>{
+
+    mongoClient.connect(connectionString).then(connectionObject=>{
+
+        var database = connectionObject.db("videodb");
+
+        var user = {
+             VideoId:parseInt(req.body.VideoId),
+             Title:req.body.Title,
+             Url:req.body.Url,
+             Description:req.body.Description,
+             Likes:parseInt(req.body.Likes),
+             Dislikes:parseInt(req.body.Dislikes),
+             Views:parseInt(req.body.Views),
+             CategoryId:parseInt(req.body.CategoryId),
+             Comments:[req.body.Comments]
+        }
+
+        database.collection("tblvideo").updateOne({VideoId:parseInt(req.params.id)},{$set:video}).then(()=>{
+             console.log(`Video Update Successfully...`);
+             res.end();
+        });
+
+    });
+});
+
+app.delete("/delete-video/:id", (req, res)=>{
+    mongoClient.connect(connectionString).then(connectionObject=>{
+        var database = connectionObject.db("videodb");
+
+        database.collection("tblvideos").deleteOne({VideoId:parseInt(req.params.id)})
+        .then(()=>{
+            console.log("Video Deleted.");
+            res.end();
+        })
+
+    })
+})
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <CookiesProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </CookiesProvider>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.listen(5050);
+console.log(`Server Started:http://127.0.0.1:5050`);
